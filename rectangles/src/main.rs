eda102f231c1533da16a76c3f47724aec99f96af
btamagne <btamagne@gmail.com>
2023-09-12 21:05:21 -0400

    implementing some methods on retangle

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,28 +4,55 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+    
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
+    let scale = 2;
+
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(15 * scale),
         height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
     };
 
     println!("rect1 is {:#?}", rect1);
+    println!("rect2 is {:#?}", rect2);
+    println!("rect3 is {:#?}", rect3);
 
+    
     println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        "The area of the rect1 is {} square pixels.",
+        rect1.area()
     );
+    
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
-    let scale = 2;
-    let rect2 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
+    let square1 = Rectangle::square(3);
 
-    dbg!(&rect2);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    dbg!(&square1);
 }
