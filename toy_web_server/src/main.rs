8ff9b387cc2f5de82a48246f97f9257271439f3c
btamagne <btamagne@gmail.com>
2023-11-12 12:43:22 -0500

    now handling the connection, logging requests

--- toy_web_server/src/main.rs
+++ toy_web_server/src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // "We’ve chosen this port for two reasons: HTTP isn’t normally accepted on
@@ -23,6 +26,17 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:?}", http_request);
 }
