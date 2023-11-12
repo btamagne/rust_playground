a0d939bb6fe6a484d33b07880942f43f3f4be4f1
btamagne <btamagne@gmail.com>
2023-11-12 13:17:51 -0500

    Sending actual html to be loaded on the page

--- toy_web_server/src/main.rs
+++ toy_web_server/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -6,15 +7,16 @@ use std::{
 fn main() {
     // "We’ve chosen this port for two reasons: HTTP isn’t normally accepted on
     // this port so our server is unlikely to conflict with any other web server
-    // you might have running on your machine, and 7878 is rust typed on a telephone."
+    // you might have running on your machine, and 7878 is rust typed on a
+    // telephone."
     
     // "The bind function in this scenario works like the new function in that
     // it will return a new TcpListener instance. The function is called bind
     // because, in networking, connecting to a port to listen to is known as
     // “binding to a port.”"
 
-    // unwrap could technically cause a panic, but only if the wrapped type is Err(E).
-    // In this case, we are confident that binding to loopback (127.0.0.1) on the
+    // unwrap could technically cause a panic, but only if the wrapped type is
+    // Err(E). In this case, we are confident that binding to loopback (127.0.0.1) on the
     // port 7878 will typically succeed, and return a Ok(T) in the option.
 
     // "Because we’re writing a basic server just for learning purposes, we
@@ -32,7 +34,7 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let _http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
@@ -40,7 +42,12 @@ fn handle_connection(mut stream: TcpStream) {
 
     //println!("Request: {:?}", http_request);
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("toy_web_server.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
