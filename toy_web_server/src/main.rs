fefee6f58ea072460a1b7a9eea82f4cbafbe6983
btamagne <btamagne@gmail.com>
2023-11-25 20:54:38 -0500

    Making the web server multithreaded

--- toy_web_server/src/main.rs
+++ toy_web_server/src/main.rs
@@ -1,45 +1,39 @@
+use toy_web_server::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
-    // "We’ve chosen this port for two reasons: HTTP isn’t normally accepted on
-    // this port so our server is unlikely to conflict with any other web server
-    // you might have running on your machine, and 7878 is rust typed on a
-    // telephone."
-    
-    // "The bind function in this scenario works like the new function in that
-    // it will return a new TcpListener instance. The function is called bind
-    // because, in networking, connecting to a port to listen to is known as
-    // “binding to a port.”"
-
-    // unwrap could technically cause a panic, but only if the wrapped type is
-    // Err(E). In this case, we are confident that binding to loopback (127.0.0.1) on the
-    // port 7878 will typically succeed, and return a Ok(T) in the option.
-
-    // "Because we’re writing a basic server just for learning purposes, we
-    // won’t worry about handling these kinds of errors; instead, we use unwrap
-    // to stop the program if errors happen."
-
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "toy_web_server.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line [..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "toy_web_server.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "toy_web_server.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
